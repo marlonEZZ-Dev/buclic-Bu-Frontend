0f263de6c94e3866bf48b12ad5d8fe29f2cb6ec6
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-09-16 13:36:19 -0500

    TopNavbar

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoUnivalleDark from '../assets/logo_univalle_dark.svg';
+import LogoUnivalleLight from '../assets/logo_univalle_light.svg';
 import PropTypes from 'prop-types';
 import '../styles/TopNavbar.css';
 
@@ -7,9 +8,24 @@ export default function TopNavbar(props) {
   const session = props.session ? 'session' : 'top-navbar';
 
   return (
-    <header id='univalle-logo-header' className={session}>
-      <Link to="/">
-        <img src={LogoUnivalleDark} alt="Logo Universidad del Valle" />
+    <header 
+      id='univalle-logo-header' 
+      className={session}
+      style={{
+        backgroundColor: '#C20E1A',
+        width: '100vw',
+        height: '90px',
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 20px',
+      }}
+    >
+      <Link to="/" style={{ marginRight: 'auto' }}>
+        <img 
+          src={LogoUnivalleLight} 
+          alt="Logo Universidad del Valle" 
+          style={{ height: '60px' }} 
+        />
       </Link>
     </header>
   );
