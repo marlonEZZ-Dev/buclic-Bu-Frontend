bfa1865d87b0d1b964509e5ed8e73da52d7a39be
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-09-18 14:34:24 -0500

    enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Fragment, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import { ConfigProvider } from "antd";
-import "antd/dist/reset.css"; 
+import "antd/dist/reset.css";
 import LoginPage from "./pages/auth/LoginPage";
 import AdminPage from "./pages/AdminPage";
 import StudentPage from "./pages/StudentPage";
+
 function App() {
   return (
     <ConfigProvider>
@@ -13,16 +14,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
 
+        <Route path="/admin/*" element={<AdminPage />} />
 
-        <Route path="/admin/*" element={<AdminPage/>} />
-
-        <Route path="/student/*" element={<StudentPage/>}/>
-
+        <Route path="/student/*" element={<StudentPage />} />
       </Routes>
-
-      
-
-      
     </ConfigProvider>
   );
 }
