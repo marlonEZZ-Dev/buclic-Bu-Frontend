0232b1616cec37a5f0748813432e6f7c142f15f2
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2024-09-18 15:48:09 -0500

    Modificación
    Se agregaron colores y la tipografía a utilizar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: [],
   theme: {
+    color : {
+      'red':'#C20E1A',
+      'gray-dark':'#636363',
+      'gray':'#CFCFCF',
+      'blue':'#3D5AA2',
+      'white':'#f4f4f4'
+    },
+    fontFamily:{
+      sans: 'Open Sans'
+    },
     extend: {},
   },
   plugins: [],
