340b66b6e39b047e93755420301d6a78987dbe9b
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-09-24 10:46:25 -0500

    Creación de archivos para admin.

---
+++ src/pages/admin/Informs.jsx
@@ -0,0 +1,10 @@
+import HeaderAdmin from "../../components/admin/HeaderAdmin.jsx";
+
+export default function Informs() {
+  return (
+    <>
+      <HeaderAdmin/>
+      {/* Aqui programan todo lo de la vista */}
+    </>
+  );
+}
