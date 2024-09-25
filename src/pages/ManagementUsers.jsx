0e13acd61aa83345137e10bd7e85a4d057ca028e
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2024-09-25 09:53:51 -0500

    Idea desechada
    Existe otra forma más rápida de hacerlo

--- src/pages/ManagementUsers.jsx
+++ src/pages/ManagementUsers.jsx
@@ -7,31 +7,11 @@ import {
 import TopNavbar from "../components/TopNavbar.jsx";
 
 export default function ManagementUsers(){
-  //const [changeTypeUser, setChangeTypeUser] = useState(false);
 
   return (
     <>
       <TopNavbar/>
-      <Row>
-        <Col span={5}></Col>
-        <Col span={14}>
-        <nav className="bg-transparent">
-          <button/>
-          <Flex 
-          justify="center" 
-          align="flex-end" 
-          gap="large">
-            <button className="rounded-t-lg">
-              Estudiantes
-            </button>
-            <button className="rounded-t-lg">
-              Funcionarios
-            </button>
-          </Flex>
-        </nav>
-        </Col>
-        <Col span={5}></Col>
-      </Row>
+      
     </>
-  )
-}
+  );
+};
