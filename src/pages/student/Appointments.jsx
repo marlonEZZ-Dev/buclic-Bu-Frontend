12acf31e41b4ca66c6186f5b19c81a00bb5308fe
Luisafcc14 <165429963+Luisafcc14@users.noreply.github.com>
2024-09-27 17:53:34 -0500

    Organización de estilos

--- src/pages/student/Appointments.jsx
+++ src/pages/student/Appointments.jsx
@@ -2,48 +2,66 @@ import React from 'react';
 import TopNavbar from '../../components/TopNavbar';
 import AppointmentMenu from '../../components/global/AppointmentMenu';
 
-
 const Appointments = () => {
   return (
     <>
       <TopNavbar />
-      <div style={{ display: 'flex', marginTop: '100px', alignContent:'center', width: '100%' }}>
-      <h1 className="text-xl font-bold #C20E1A">Área de citas</h1>
-
-      </div>
       
-      
-      <div style={{ display: 'flex', marginTop: '9px', justifyContent: 'space-between', width: '100%' }}>
-        <AppointmentMenu
-          image="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" // Cambia la URL según necesites
-          title="Psicología"
-          style={{
-            backgroundColor: 'white', // Fondo azul
-            borderRadius: '2px', // Bordes de 8px
-            width: '329px', 
-          }}
-        />
+      {/* Contenido principal */}
+      <main style={{ marginTop: '100px', padding: '0 20px' }}> 
+        
+        {/* Título principal */}
+        <header>
+          <h1 className="text-xl font-bold" style={{ color: '#C20E1A', textAlign: 'center' }}>Área de citas</h1>
+        </header>
+        
+        {/* Sección de opciones de citas */}
+        <section style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px', flexWrap: 'wrap' }}>
+          
+          {/* Opción 1: Psicología */}
+          <article>
+            <AppointmentMenu
+              image="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" // Cambia la URL según necesites
+              title="Psicología"
+              style={{
+                backgroundColor: 'white', // Fondo blanco
+                borderRadius: '8px', // Bordes redondeados
+                width: '329px', 
+                marginBottom: '20px', // Espacio entre las tarjetas en pantallas pequeñas
+              }}
+            />
+          </article>
 
-        <AppointmentMenu
-          image="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" // Cambia la URL según necesites
-          title="Enfermería - Médico general"
-          style={{
-            backgroundColor: 'white', // Fondo azul
-            borderRadius: '2px', // Bordes de 8px
-            width: '329px', 
-          }}
-        />
+          {/* Opción 2: Enfermería - Médico general */}
+          <article>
+            <AppointmentMenu
+              image="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" // Cambia la URL según necesites
+              title="Enfermería - Médico general"
+              style={{
+                backgroundColor: 'white', // Fondo blanco
+                borderRadius: '8px', // Bordes redondeados
+                width: '329px', 
+                marginBottom: '20px',
+              }}
+            />
+          </article>
 
-        <AppointmentMenu
-          image="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" // Cambia la URL según necesites
-          title="Odontología"
-          style={{
-            backgroundColor: 'white', // Fondo azul
-            borderRadius: '2px', // Bordes de 8px
-            width: '329px', 
-          }}
-        />
-      </div>
+          {/* Opción 3: Odontología */}
+          <article>
+            <AppointmentMenu
+              image="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" // Cambia la URL según necesites
+              title="Odontología"
+              style={{
+                backgroundColor: 'white', 
+                borderRadius: '8px', 
+                width: '329px', 
+                marginBottom: '20px',
+              }}
+            />
+          </article>
+          
+        </section>
+      </main>
     </>
   );
 };
--- src/pages/student/ChangePassword.jsx
+++ src/pages/student/ChangePassword.jsx
@@ -1,93 +1,82 @@
-import React from 'react'; // Importar React
-import TopNavbar from '../../components/TopNavbar'; // Importar el componente de navegación
-import { Card, Flex, Input, Space, Button } from 'antd'; // Importa todos los componentes necesarios de 'antd'
-
+import React from 'react';
+import TopNavbar from '../../components/TopNavbar';
+import { Card, Space, Button, Form, Input } from 'antd';
 
 const ChangePassword = () => {
   return (
     <>
       <TopNavbar />
-      <Card
-        bordered={true}
-        style={{
-          width: '400px',
-          marginTop: '110px',
-          margin: '3px auto',
-          justifyContent: 'center',
-          display: 'flex',
-          flexDirection: 'column', // Asegúrate de que los elementos se coloquen en columna
-          alignItems: 'center', // Centra horizontalmente
-          justifyContent: 'center', // Centra verticalmente
-       
-        }}
-      >
 
-        <Space style={{ marginTop: '8px' }}>
-          <h1 style={{ margin: 0, fontSize: 19 }}>Cambiar contraseña</h1>
-        </Space>
+      {/* Contenido principal */}
+      <main style={{ marginTop: '100px', padding: '0 20px', display: 'flex', justifyContent: 'center' }}>
+
+        <Card
+          bordered={true}
+          style={{
+              width: '500px',
+              marginTop: '100px',
+              margin: '3px auto',
+              justifyContent: 'center',
+          }}
+        >
+
+          {/* Título */}
+          <Space style={{ marginTop: '5px',alignItems: 'center' }}>
+          <h1 className="titleCard">Cambiar contraseña</h1>
+          </Space>
+
+          
+          <p>La contraseña debe contener mínimo 8 caracteres.</p>
+
+          {/* Formulario de cambio de contraseña */}
+          <Form layout="vertical" style={{ marginTop: '8px' }} >
+
+              {/* Campo Contraseña actual */}
+              <Form.Item label="Contraseña">
+                <Input.Password id="current-password" placeholder="Contraseña actual" />
+              </Form.Item>
+
+              {/* Campo Nueva contraseña */}
+              <Form.Item label="Nueva contraseña">
+                <Input.Password id="current-password" placeholder="Nueva contraseña" />
+              </Form.Item>
+
+              {/* Campo Confirmar nueva contraseña */}
+              <Form.Item label="Confirmar nueva contraseña">
+                <Input.Password id="current-password" placeholder="Confirmar nueva contraseña" />
+              </Form.Item>
 
 
-        <Space direction="vertical" style={{ marginTop: '30px', display: 'flex', alignItems: 'flex-start' }} >
 
-          <label style={{ width: '100%' }}>Contraseña actual</label>
-          <Input.Password placeholder="input password" />
+            {/* Botones */}
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '10px', marginTop: '20px' }}>
 
-          <label style={{ width: '100%' }}>Nueva contraseña</label>
-          <Input.Password placeholder="input password" />
+              {/* Botón Guardar */}
+              <Button
+                type="default"
+                htmlType="submit"
+                className="button-save"
+              >
+                Guardar
+              </Button>
 
-          <label style={{ width: '100%' }}>Confirmar nueva contraseña</label>
-          <Input.Password placeholder="input password" />
+              {/* Botón Cancelar */}
+              <Button
+                type="default"
+                htmlType="reset"
+                className="button-cancel"
+              >
+                Cancelar
+              </Button>
 
-        </Space>
+            </div>
+          </Form>
 
-        <div style={{ display: 'flex', justifyContent: 'center', gap: '10px', marginTop: '10px' }}>
-          <Button
-            type="default"
-            style={{
-              backgroundColor: '#C20E1A',
-              color: '#FFFFFF',
-              border: 'none',
-              height: '30px',
-              width: '149px',
-            }}
-            onMouseEnter={(e) => {
-              e.currentTarget.style.backgroundColor = '#841F1C';
-            }}
-            onMouseLeave={(e) => {
-              e.currentTarget.style.backgroundColor = '#C20E1A';
-            }}
-          >
-            Guardar
-          </Button>
-
-          <Button
-            type="default"
-            style={{
-              backgroundColor: '#FFFFFF', // Cambia a blanco
-              color: '#C20E1A', // Color del texto rojo
-              border: 'none', // Elimina el borde
-              height: '30px',
-              width: '149px',
-              marginTop: '80 px',
-              transition: 'background-color 0.3s, color 0.3s', // Transición suave
-            }}
-            onMouseEnter={(e) => {
-              e.currentTarget.style.backgroundColor = '#841F1C'; // Cambia a rojo oscuro
-              e.currentTarget.style.color = '#FFFFFF'; // Cambia el texto a blanco
-            }}
-            onMouseLeave={(e) => {
-              e.currentTarget.style.backgroundColor = '#FFFFFF'; // Vuelve a blanco
-              e.currentTarget.style.color = '#C20E1A'; // Vuelve el texto a rojo
-            }}
-          >
-            Cancelar
-          </Button>
-
-        </div>
-
-      </Card >
+        </Card>
+      </main>
     </>
   );
 };
 
 export default ChangePassword;
+
