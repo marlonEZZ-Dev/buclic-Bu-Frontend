95be15c4fbd422ddba90d601d110521e36fc807b
Luisafcc14 <165429963+Luisafcc14@users.noreply.github.com>
2024-09-27 18:43:31 -0500

    Estilos y reserva con nuevos elementos

--- src/pages/admin/Reservations.jsx
+++ src/pages/admin/Reservations.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import HeaderAdmin from "../../components/admin/HeaderAdmin.jsx";
 import SearchInput from '../../components/global/SearchInput.jsx';
-import { Card, Space, Button, Input} from 'antd';
+import { Card, Space, Button, Descriptions } from 'antd';
 
 const Reservations = () => {
+
+    const person = {
+        name: 'John Doe',
+        age: 25,
+        email: 'john.doe@example.com',
+        reservationDate: '2024-09-15',
+    };
     return (
         <>
-            <HeaderAdmin/>
+            <HeaderAdmin />
             <main style={{ marginTop: '100px', padding: '0 20px', display: 'flex', justifyContent: 'center' }}>
                 <Card
                     bordered={true}
@@ -19,7 +26,30 @@ const Reservations = () => {
                     <p>Aquí puedes buscar las personas que han reservado la beca de alimentación.</p>
 
 
-                    <SearchInput />
+                    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                        <SearchInput />
+                    </div>
+
+                    {/* Información de la persona con Descriptions */}
+                    <Descriptions title="Información de la Persona" bordered column={1} style={{ marginTop: '15px' }}>
+
+                        <Descriptions.Item label="Nombre" className="descriptions-item">
+                            {person.name}
+                        </Descriptions.Item>
+
+                        <Descriptions.Item label="Edad" className="descriptions-item">
+                            {person.age}
+                        </Descriptions.Item>
+
+                        <Descriptions.Item label="Correo" className="descriptions-item">
+                            {person.email}
+                        </Descriptions.Item>
+                        
+                        <Descriptions.Item label="Fecha de Reserva" className="descriptions-item">
+                            {person.reservationDate}
+                        </Descriptions.Item>
+                    </Descriptions>
+
 
 
                     <div style={{ display: 'flex', justifyContent: 'center', gap: '10px', marginTop: '20px' }}>
