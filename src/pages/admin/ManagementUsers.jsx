6678a3db841b0e335926ce6e7073d1af68cde068
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2024-09-28 16:17:26 -0500

    Falta paginación

--- src/pages/admin/ManagementUsers.jsx
+++ src/pages/admin/ManagementUsers.jsx
@@ -9,14 +9,14 @@ import styles from "../../styles/admin/managementUsers.module.css"
 
 export default function ManagementUsers(){
   
+  //Definicion de variables
   const buttons = [
     {type:"Beneficiarios",label:"Beneficiarios"},
     {type:"Estudiantes",label:"Estudiantes"},
     {type:"Funcionarios",label:"Funcionarios"}
   ]
-
-  const [changesDescription, setChangesDescription] = useState(0)
-  const [selectedType, setSelectedType] = useState("Estudiantes")
+  
+  const users = new Map(buttons.map( (obj, index) => [obj.type, index]))
   
   const descriptions = [
     {
@@ -29,9 +29,34 @@ export default function ManagementUsers(){
     },
     {
       title:"Funcionarios del sistema",
-      description:"Aquí puedes agregar personas con alguna dependencia en la universidado externas"
+      description:"Aquí puedes agregar personas con alguna dependencia en la universidad o externas"
     }
   ]
+
+  const cbxStudents = [
+    {value:"almuerzo", label:"Beneficiario almuerzo"},
+    {value:"refrigerio", label:"Beneficiario refrigerio"}
+  ]
+  
+  const cbxFuncionary = [
+    {value:"administrador", label:"Administrador (a)"},
+    {value:"enfermero", label:"Enfermero (a)"},
+    {value:"monitor", label:"Monitor (a)"},
+    {value:"odontologo", label:"Odontólogo (a)"},
+    {value:"psicologo", label:"Psicólogo (a)"}
+  ]
+
+  //useStates
+  const [changesDescription, setChangesDescription] = useState(0)
+  
+  //Handlers
+  const handlerClick = type => {
+    setChangesDescription(users.get(type))
+  }
+
+  //Predicados
+  let isStudent = changesDescription === 1
+  let isFuncionary = changesDescription === 2
   
   return (
     <>
@@ -39,7 +64,7 @@ export default function ManagementUsers(){
       <div className={styles.menuGrant}>
         <MenuBecas 
         buttons={buttons}
-        selectedType={selectedType}>
+        onSelect={type => handlerClick(type)}>
         <button className={styles.buttonImport}>
           Importar
         </button>
@@ -57,57 +82,56 @@ export default function ManagementUsers(){
           
           <Flex gap={29}>
           <label className={styles.labels}>
-              Nombre <span>*</span>
+              Nombre <span className={styles.asteric}>*</span>
               <input 
               type="text"
               className={styles.inputs} 
-              placeholder='Nombre(s) del estudiante'/>
+              placeholder={`Nombre(s) ${isFuncionary ? "de la persona" : "del estudiante"}`}/>
             </label>
             <label className={styles.labels}>
-              Apellidos <span>*</span>
+              Apellidos <span className={styles.asteric}>*</span>
               <input 
               type="text" 
               className={styles.inputs}
-              placeholder='Apellidos del estudiante'/>
+              placeholder={`Apellidos ${isFuncionary ? "de la persona" : "del estudiante"}`}/>
             </label>
           </Flex>
 
           <Flex gap={29}>
           <label className={styles.labels}>
-              Código estudiantil <span>*</span>
+              {isFuncionary ? "Cédula" : "Código estudiantil"} <span className={styles.asteric}>*</span>
               <input 
               type="text" 
               className={styles.inputs}
-              placeholder='Código del estudiante'/>
+              placeholder={isFuncionary ? "Cédula de la persona":"Código del estudiante"}/>
             </label>
             <label className={styles.labels}>
-              Plan <span>*</span>
+              {isFuncionary ? "Área dependiente" : 
+              <span>Plan <span className={styles.asteric}>*</span></span>}
               <input 
               type="text" 
               className={styles.inputs}
-              placeholder='Plan del estudiante'/>
+              placeholder={ isFuncionary ? "Área de la persona":'Plan del estudiante'}/>
             </label>
           </Flex>
           
           <Flex gap={29}>
           <label className={styles.labels}>
-              Correo electrónico <span>*</span>
+              Correo electrónico <span className={styles.asteric}>*</span>
               <input 
               type="text" 
               className={styles.inputs}
               placeholder='Correo del estudiante'/>
-            </label>
-            <label className={styles.labels}>
-              Tipo de beca
-              <Select
-              placeholder="Selecciona"
-              className={styles.comboboxes}
-              options={[
-                {value:"almuerzo", label:"Beneficiario almuerzo"},
-                {value:"refrigerio", label:"Beneficiario refrigerio"}
-              ]}
-              />
-            </label>
+          </label>
+          <label className={`${styles.labels} ${isStudent ? "visibility-hidden" : ""}`}>
+            {isFuncionary ? "Rol" : 
+            <span>Tipo de beca <span className={styles.asteric}>*</span></span>}          
+          <Select
+          placeholder="Selecciona"
+          className={styles.comboboxes}
+          options={isFuncionary ? cbxFuncionary : cbxStudents}
+          />
+          </label>
           </Flex>
         </Flex>
 
