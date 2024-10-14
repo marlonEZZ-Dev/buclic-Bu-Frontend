b4e5bec63ea4b8ef32bb747bbe558e79c6d1f294
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-10-14 11:46:04 -0500

    Configuracion para decodificar

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(),],
-  css:{
-    modules:{
+  plugins: [react(), svgr()],
+  css: {
+    modules: {
       generateScopedName: '[name]__[local]___[hash:base64:5]'
     }
-  }
-})
+  },
+  optimizeDeps: {
+    include: ['jwt-decode'],  // Aseguramos que jwt-decode sea optimizado
+  },
+});
