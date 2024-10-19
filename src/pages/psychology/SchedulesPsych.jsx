e3e85b31fed6dff960d9cbfce52abd6114583d63
marlonEZZ-Dev <duendecum@gmail.com>
2024-10-19 17:18:19 -0500

    Agrengando la modal

--- src/pages/psychology/SchedulesPsych.jsx
+++ src/pages/psychology/SchedulesPsych.jsx
@@ -1,8 +1,11 @@
 import styles from "../../styles/psychology/schedulesPsych.module.css";
 import HeaderPsych from "../../components/psychology/HeaderPsych.jsx";
+import FooterProfessionals from "../../components/global/FooterProfessionals.jsx";
+import Modal from "../../components/global/Modal.jsx";
 import locale from "antd/es/locale/es_ES";
 import dayjs from "dayjs";
 import {Flex} from "antd"
+import { InfoCircleOutlined } from "@ant-design/icons"
 import buddhistEra from "dayjs/plugin/buddhistEra";
 dayjs.extend(buddhistEra);
 import PropTypes from "prop-types"
@@ -81,34 +84,34 @@ function TableEditable(){
   return (
     <>
     <table className={styles.cssTable} border="1">
-          <thead>
-            <tr>
-              <th>Fecha</th>
-              <th>Hora</th>
-            </tr>
-          </thead>
-          <tbody>
-            {scheduleData.map((schedule, dateIndex) => (
-              <>
-                {/* Fila principal de la fecha */}
-                <tr key={`date-${dateIndex}`}>
-                  {/* Celda de la fecha */}
-                  <td>
-                    <DateComponent
-                      value={schedule.date}
-                      onChange={(date) => handlerDateChange(date, dateIndex)}
-                    />
-                  </td>
-                  {/* Primer campo de hora */}
-                  <td>
-                    <TimeComponent
-                      value={schedule.times[0]}
-                      onChange={(time) =>
-                        handlerTimeChange(time, dateIndex, 0)
-                      }
-                    />
-                  </td>
-                </tr>
+      <thead>
+        <tr>
+          <th>Fecha</th>
+          <th>Hora</th>
+        </tr>
+      </thead>
+      <tbody>
+        {scheduleData.map((schedule, dateIndex) => (
+        <>
+       {/* Fila principal de la fecha */}
+          <tr key={`date-${dateIndex}`}>
+          {/* Celda de la fecha */}
+            <td>
+              <DateComponent
+                value={schedule.date}
+                onChange={(date) => handlerDateChange(date, dateIndex)}
+              />
+            </td>
+             {/* Primer campo de hora */}
+            <td>
+              <TimeComponent
+                value={schedule.times[0]}
+                onChange={(time) =>
+                  handlerTimeChange(time, dateIndex, 0)
+                }
+              />
+            </td>
+          </tr>
 
                 {/* Fila para las horas adicionales (si hay más de una hora) */}
                 {schedule.times.slice(1).map((time, timeIndex) => (
@@ -155,20 +158,75 @@ function TableEditable(){
 }
 
 export default function SchedulesPsych() {
-
+  const [saveChanges, setSaveChanges] = useState({status: false, title: "", content: "", save: false})
   const [onPressEdit, setOnPressEdit] = useState(false)
 
   const handlerEdit = () => setOnPressEdit(true)
+
+  const handlerSaveChangesFill = ({title, content, save}) => {
+    setSaveChanges({
+      status: true,
+      title: title,
+      content: content,
+      save: save
+    })
+  }
+
+  const handlerSaveChangesClose = () => {
+    setSaveChanges( obj => ({
+      ...obj,
+      status: false
+    }))
+  }
+
   const handlerDontEdit = () => setOnPressEdit(false)
+  
+  const handlerBtnSave = () => {
+    handlerSaveChangesFill({
+      title:"Guardar Cambios",
+      content:"¿Desea guardar estos cambios?",
+      save: true})
+  }
+  
+  const handlerBtnCancel = () => {
+    handlerSaveChangesFill({
+      title:"Sin guardar cambios",
+      content:"¿Desea irse sin guardar cambios?",
+      save: false
+    })
+  }
 
   return (
     <>
       <HeaderPsych />
+      <Modal
+      open={saveChanges.status}
+      footer={null}
+      onClose={handlerSaveChangesClose}
+      >
+        <Flex vertical justify="center" align="center">
+          <span className={styles.modalFontSizeTitle}><InfoCircleOutlined />  {saveChanges.title}</span>
+        </Flex>
+        <Flex vertical justify="flex-start" align="center">
+          <p className={styles.modalFontSizeContent}>{saveChanges.content}</p>
+        </Flex>
+        <Flex justify="center" align="center" gap={20}>
+          <button 
+          className="button-cancel" 
+          onClick={handlerSaveChangesClose}>No</button>
+          <button 
+          className="button-save"
+          onClick={() => {
+              handlerSaveChangesClose()
+              handlerDontEdit()
+              }}>Si</button>
+        </Flex>
+      </Modal>
       <div className={`${styles.contentTitle} text-red`}>
         <h1>Definir Horarios</h1>
         <p>Aquí se puede definir el horario para citas disponibles</p>
       </div>
-      <Flex justify="center" align="center">
+      <Flex justify="center" align="center" className={styles.tableWrapper}>
         {onPressEdit ? <TableEditable/>:<>
         <table className={styles.cssTable} border="1">
           <thead>
@@ -186,13 +244,16 @@ export default function SchedulesPsych() {
         </table>
         </>}
         </Flex>
-        <Flex justify="center" align="center">
-          <button className={`button-save ${styles.marginTopEdit}`} onClick={handlerEdit}>Editar</button>
+        <Flex justify="center" align="center" className={styles.marginTopEdit} gap={30}>
+          {!onPressEdit && <button className="button-save" onClick={handlerEdit}>Editar</button>}
           {onPressEdit && <>
-          <button className="button-save">Guardar</button>
-          <button className="button-cancel" onClick={handlerDontEdit}>Cancelar</button>
+          <button 
+            className="button-save" 
+            onClick={handlerBtnSave}>Guardar</button>
+          <button className="button-cancel" onClick={handlerBtnCancel}>Cancelar</button>
           </>}
         </Flex>
+        <FooterProfessionals/>
     </>
   );
 }
