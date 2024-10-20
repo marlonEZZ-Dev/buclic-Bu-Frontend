ff7b5535c69df740f0540c3a17c35cfd09464a2a
marlonEZZ-Dev <duendecum@gmail.com>
2024-10-20 10:36:28 -0500

    Estructura inicial... Componetizando DatePicker y TimePicker para poder reutilizar aquí

--- src/pages/nurse/VisitsNurse.jsx
+++ src/pages/nurse/VisitsNurse.jsx
@@ -1,7 +1,45 @@
-import HeaderNurse from "../../components/nurse/HeaderNurse";
+import HeaderNurse from "../../components/nurse/HeaderNurse.jsx";
+import {Card, Flex, Select, TextArea} from "antd";
+import SmallInput from "../../components/global/SmallInput.jsx"; 
 
 export default function BecasNurse(){
-    return(
-        <HeaderNurse/>
-    );
+  return(
+    <>
+      <HeaderNurse/>
+      <Flex justify="center" align="center">
+        <span>Registro de actividades</span>
+        <p>Aquí se podrán registrar las actividades de usuarios al servicio</p>
+      </Flex>
+      <Card>
+      <Flex>
+        <Flex>
+          <SmallInput/>
+          <SmallInput/>
+        </Flex>
+        <Flex>
+          <SmallInput/>
+          <SmallInput/>
+        </Flex>
+        <Flex>
+          <SmallInput/>
+          <SmallInput/>
+        </Flex>
+        <Flex>
+          <label>
+            <Select/>
+          </label>
+          <label>
+            <Select/>
+          </label>
+        </Flex>
+        <Flex>
+          <label>
+            <TextArea></TextArea>
+          </label>
+        </Flex>
+      </Flex>
+      </Card>
+
+    </>
+  );
 }
