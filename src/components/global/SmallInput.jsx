5bb0ed1218713d581f65d8d93c288536e8bbc75a
marlonEZZ-Dev <duendecum@gmail.com>
2024-10-26 08:23:40 -0500

    Modificando el componente Input small

--- src/components/global/SmallInput.jsx
+++ src/components/global/SmallInput.jsx
@@ -1,27 +1,33 @@
-import styles from "../../styles/global/inputSmall.module.css"
+  import styles from "../../styles/global/inputSmall.module.css"
 
-import PropTypes from "prop-types"
+  import PropTypes from "prop-types"
 
-export default function InputSmall({
-  isRenderAsteric= true,
-  labelClassname= "",
-  title= "title",
-  ...props
-}){
-  return(
-    <label className={`${styles.labels} ${labelClassname}`}>
-      {title} {isRenderAsteric ? <span className={styles.asteric}>*</span> : ""}
-      <input 
-      type="text"
-      className={styles.inputs}
-      {...props}/>
-    </label>
-    )
-}
+  export default function InputSmall({
+    isRenderAsteric= true,
+    labelClassname="",
+    title="title",
+    ...props
+  }){
 
-InputSmall.propTypes = {
-  isRenderAsteric: PropTypes.bool,
-  labelClassname: PropTypes.string,
-  placeholder: PropTypes.string,
-  title: PropTypes.string
-}
+    return(
+      <label className={`${styles.labels} ${labelClassname}`}>
+        {title} {isRenderAsteric ? <span className={styles.asteric}>*</span> : ""}
+        <input 
+        {...props}
+        type={"type" in props ? props.type : "text"}
+        className={`${styles.inputs} ${props.className}`}
+        />
+      </label>
+      )
+  }
+
+  InputSmall.propTypes = {
+    isRenderAsteric: PropTypes.bool,
+    labelClassname: PropTypes.string,
+    placeholder: PropTypes.string,
+    title: PropTypes.string,
+    props: PropTypes.shape({
+      className: PropTypes.string,
+      type: PropTypes.string
+    })
+  }
