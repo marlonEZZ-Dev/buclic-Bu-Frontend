af868a2afb87494da23f104d7da585d059876b19
Luisafcc14 <165429963+Luisafcc14@users.noreply.github.com>
2024-11-05 11:09:19 -0500

    Ajustes de perfil todos los roles

---
+++ src/pages/monitor/PasswordMonitor.jsx
@@ -0,0 +1,235 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../../api';
+import HeaderMonitor from "../../components/monitor/HeaderMonitor";
+import { Card, Space, Button, Form, Input, message } from 'antd';
+import { InfoCircleOutlined, ArrowLeftOutlined } from '@ant-design/icons';
+import Modal from '../../components/global/Modal';
+
+const PasswordMonitor = () => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false); // Estado para el modal
+  const [confirmLoading, setConfirmLoading] = useState(false); // Estado para la carga del botón de confirmar
+  const [cancelModalVisible, setCancelModalVisible] = useState(false); // Estado para el modal de cancelación
+  const navigate = useNavigate();
+
+  // Obtener el nombre de usuario del almacenamiento local
+  const username = localStorage.getItem('username');
+
+  // Función que se llama al confirmar el cambio de contraseña
+  const handleConfirmChangePassword = async (values) => {
+    try {
+      setConfirmLoading(true);
+
+      const response = await api.post('/users/changePassword', {
+        username: username, // Incluir el nombre de usuario
+        password: values.currentPassword,
+        newPassword: values.newPassword,
+        confirmPassword: values.confirmPassword,
+      });
+
+      message.success(response.data.message || 'Contraseña cambiada con éxito');
+      form.resetFields();
+    } catch (error) {
+      message.error(error.response?.data?.message || 'Error al cambiar la contraseña');
+    } finally {
+      setConfirmLoading(false);
+      setModalVisible(false); // Cierra el modal después de confirmar
+    }
+  };
+
+  // Función que se llama al enviar el formulario
+  const onFinish = (values) => {
+    setModalVisible(true); // Mostrar modal de confirmación
+  };
+
+  // Nueva función para manejar la cancelación
+  const handleCancel = () => {
+    setCancelModalVisible(true); // Mostrar modal de confirmación de cancelación
+  };
+
+  const confirmCancel = () => {
+    setCancelModalVisible(false);
+    handleBack(); // Llama a la función de navegación
+  };
+
+  const handleBack = () => {
+    navigate('/monitor/ajustes');
+  };
+
+  return (
+    <>
+      <HeaderMonitor />
+
+      {/* Contenido principal */}
+      <main style={styles.main}>
+
+
+        <Button type="default" icon={<ArrowLeftOutlined style={{ color: '#fff' }} />} className="button-save"
+          onClick={handleBack}>
+        </Button>
+
+        <Card bordered={true} style={styles.card}>
+          {/* Título */}
+          <Space style={styles.titleSpace}>
+            <h1 className="titleCard">Cambiar contraseña</h1>
+          </Space>
+
+          <p style={styles.paragraph}>La contraseña debe contener mínimo 8 caracteres.</p>
+
+          {/* Formulario de cambio de contraseña */}
+          <Form
+            form={form}
+            layout="vertical"
+            style={styles.form}
+            onFinish={onFinish}
+          >
+            {/* Campo Contraseña actual */}
+            <Form.Item
+              label="Contraseña actual"
+              name="currentPassword"
+              rules={[{ required: true, message: 'Ingresa tu contraseña actual' }]}
+            >
+              <Input.Password placeholder="Contraseña actual" />
+            </Form.Item>
+
+            {/* Campo Nueva contraseña */}
+            <Form.Item
+              label="Nueva contraseña"
+              name="newPassword"
+              rules={[{ required: true, message: 'Ingresa la nueva contraseña' }]}
+            >
+              <Input.Password placeholder="Nueva contraseña"
+                onPaste={(e) => e.preventDefault()} // Evita pegar en el campo
+              />
+            </Form.Item>
+
+            {/* Campo Confirmar nueva contraseña */}
+            <Form.Item
+              label="Confirmar nueva contraseña"
+              name="confirmPassword"
+              dependencies={['newPassword']}
+              rules={[
+                { required: true, message: 'Confirma tu nueva contraseña' },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('newPassword') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('Las contraseñas no coinciden'));
+                  },
+                }),
+              ]}
+            >
+              <Input.Password placeholder="Confirmar nueva contraseña"
+                onPaste={(e) => e.preventDefault()} // Evita pegar en el campo
+              />
+            </Form.Item>
+
+            {/* Botones */}
+            <div style={styles.buttonContainer}>
+              {/* Botón Guardar */}
+              <Button className="button-save" type="default" htmlType="submit" loading={loading} >
+                Guardar
+              </Button>
+
+              {/* Botón Cancelar */}
+              <Button className="button-cancel" type="default" htmlType="reset" onClick={handleCancel} >
+                Cancelar
+              </Button>
+            </div>
+          </Form>
+
+          {/* Modal de confirmación */}
+          <Modal
+            open={modalVisible}
+            onClose={() => setModalVisible(false)}
+            modalTitle={
+              <span>
+                <InfoCircleOutlined style={{ color: '#faad14', marginRight: '8px', fontSize: '24px' }} /> {/* Icono con color de advertencia */}
+                <span style={{ fontWeight: 'bold' }}>Confirmar cambio de contraseña</span> {/* Texto en negrita */}
+              </span>
+            }
+          >
+            <p style={{ textAlign: 'left', paddingLeft: '31px' }}>¿Estás seguro de cambiar la contraseña?</p>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', gap: '10px' }}>
+              <Button className="button-cancel" onClick={() => setModalVisible(false)}>Cancelar</Button>
+              <Button
+                className="button-save"
+                type="primary"
+                loading={confirmLoading}
+                onClick={() => {
+                  const values = form.getFieldsValue(); // Obtener los valores del formulario
+                  handleConfirmChangePassword(values); // Llamar a la función para confirmar el cambio de contraseña
+                }}
+              >
+                Guardar
+              </Button>
+            </div>
+          </Modal>
+
+          {/* Modal de confirmación de cancelación */}
+          <Modal
+            open={cancelModalVisible}
+            onClose={() => setCancelModalVisible(false)}
+            modalTitle={
+              <span>
+                <InfoCircleOutlined style={{ color: '#faad14', marginRight: '8px', fontSize: '24px' }} />
+                <span style={{ fontWeight: 'bold' }}>Confirmar cancelación</span>
+              </span>
+            }
+          >
+            <p style={{ textAlign: 'left', paddingLeft: '31px' }}>¿Estás seguro de cancelar el cambio de contraseña?</p>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', gap: '10px' }}>
+              <Button className="button-cancel" onClick={() => setCancelModalVisible(false)}>Cancelar</Button>
+              <Button
+                className="button-save"
+                type="primary"
+                onClick={confirmCancel}
+              >
+                Confirmar
+              </Button>
+            </div>
+          </Modal>
+        </Card>
+      </main>
+    </>
+  );
+};
+
+// Estilos en línea
+const styles = {
+  main: {
+    marginTop: '100px',
+    padding: '0 20px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  card: {
+    width: '100%',
+    maxWidth: '500px',
+    marginTop: '100px',
+    margin: '3px auto',
+    justifyContent: 'center',
+  },
+  titleSpace: {
+    marginTop: '5px',
+    alignItems: 'center',
+  },
+  paragraph: {
+    textAlign: 'center',
+    margin: '10px 0',
+  },
+  form: {
+    marginTop: '8px',
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '10px',
+    marginTop: '20px',
+  },
+};
+
+export default PasswordMonitor;
--- src/pages/monitor/SettingMonitor.jsx
+++ src/pages/monitor/SettingMonitor.jsx
@@ -1,7 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import HeaderMonitor from "../../components/monitor/HeaderMonitor";
+import { Button, Form, Input, message, Card } from 'antd';
+import api from '../../api';
 
-export default function SettingMonitor(){
-    return(
-        <HeaderMonitor/>
+const SettingMonitor = () => {
+    const navigate = useNavigate();
+    const [profileData, setProfileData] = useState(null);
+
+    const fetchProfileData = async () => {
+        try {
+            const username = localStorage.getItem('username');
+            if (username) {
+                const response = await api.get(`/users/${username}`);
+                setProfileData(response.data); // Guardar los datos del perfil
+            }
+        } catch (error) {
+            message.error('Error al cargar el perfil del usuario.');
+        }
+    };
+
+    useEffect(() => {
+        fetchProfileData();
+    }, []);
+
+    const handleChangePasswordClick = () => {
+        navigate('/monitor/contrasena');
+    };
+
+    return (
+        <>
+            <HeaderMonitor />
+            <main style={{ marginTop: '100px', padding: '0 20px', display: 'flex', justifyContent: 'center' }}>
+                <Card
+                    title="Perfil"
+                    bordered={false}
+                    style={{ width: '100%', maxWidth: '600px' }}
+                >
+                    {profileData ? (
+                        <Form layout="vertical">
+                            <Form.Item label={<span style={{ color: 'black', }}>Nombres</span>}>
+                                <Input value={profileData.name} disabled style={{ color: '#767676' }} />
+                            </Form.Item>
+                            <Form.Item label={<span style={{ color: 'black' }}>Apellidos</span>}>
+                                <Input value={profileData.lastName} disabled style={{ color: '#767676' }} />
+                            </Form.Item>
+                            <Form.Item label={<span style={{ color: 'black' }}>Correo</span>}>
+                                <Input value={profileData.email} disabled style={{ color: '#767676' }} />
+                            </Form.Item>
+                            <Form.Item label={<span style={{ color: 'black' }}>Tipo de beneficio</span>}>
+                                <Input value={profileData.benefitType} disabled style={{ color: '#767676' }} />
+                            </Form.Item>
+                            <div style={{ display: 'flex', justifyContent: 'left', marginTop: '20px' }}>
+                                <Button
+                                    className="button-save"
+                                    style={{ width: '180px' }}
+                                    type="primary"
+                                    onClick={handleChangePasswordClick}
+                                >
+                                    Cambiar contraseña
+                                </Button>
+                            </div>
+                        </Form>
+                    ) : (
+                        <p>Por favor selecciona una opción de perfil para ver los datos.</p>
+                    )}
+                </Card>
+            </main>
+        </>
     );
-}
+};
+
+export default SettingMonitor;
