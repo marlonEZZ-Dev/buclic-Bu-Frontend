1ce0950426a602be8f8546de8141b6dc2a67a996
Luisafcc14 <165429963+Luisafcc14@users.noreply.github.com>
2024-11-05 22:08:31 -0500

    Diseño actividades enfermería

--- src/pages/nurse/VisitsNurse.jsx
+++ src/pages/nurse/VisitsNurse.jsx
@@ -1,225 +1,161 @@
+import React, { useEffect, useState } from 'react';
 import HeaderNurse from "../../components/nurse/HeaderNurse.jsx";
-import SmallInput from "../../components/global/SmallInput.jsx"; 
-import DateSpanish from "../../components/global/DateSpanish.jsx";
-import ComboBox from "../../components/global/ComboBox.jsx";
-import TextArea from "../../components/global/TextArea.jsx";
-import FooterProfessionals from "../../components/global/FooterProfessionals.jsx";
-import Modal from "../../components/global/Modal.jsx"
+import esLocale from 'antd/es/date-picker/locale/es_ES';
+import SearchInputR from '../../components/global/SearchInputR.jsx';
+import { DatePicker, Form, Card, Space, Input, Select, Row, Col, Button, message } from "antd";
 
-import styles from "../../styles/Nurse/VisitsNurser.module.css"
-import { inputs } from "../../styles/global/inputSmall.module.css"
-import { comboboxes } from "../../styles/admin/managementUsers.module.css"
 
-import {Card, Flex} from "antd";
-import { useEffect, useState } from "react";
-import { validCodeOrNuip, validDate, validFullName, validListEmpty, validPhone, validPlanOrArea, validSemester } from "../../services/validations.js";
+export default function VisitsNurse() {
 
-export default function VisitsNurse(){
-  const [deviceType, setDeviceType] = useState("")
-  const [defineModal, setDefineModal] = useState({status:false, title:"", content:""})
+  const [fecha, setFecha] = useState(null);
+  const [nombre, setNombre] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [planDependencia, setPlanDependencia] = useState('');
+  const [semestre, setSemestre] = useState('');
+  const [conduca, setConduca] = useState('');
 
-  const cbxDiagnostic = [
-    {value:"colicoMenstrual", label:"Cólicos Menstruales"},
-    {value:"curacion", label:"Curación"},
-    {value:"dolorCabeza", label:"Dolor de cabeza"},
-    {value:"dolorEstomacal", label:"Dolor estomacal"},
-    {value:"dolorMuscular", label:"Dolor muscular"},
-    {value:"mareosDesmayos", label:"Mareos / Desmayos"},
-    {value:"preservativos", label:"Preservativos"}
-  ]
+  const onChangeFecha = (date, dateString) => {
+    setFecha(dateString);
+    console.log("Fecha seleccionada:", dateString);
+  };
 
-  const cbxGender = [
-    {value:"m", label:"Masculino"},
-    {value:"f", label:"Femenino"}
-  ]
-
-  const handleResize = () => {
-    const width = window.innerWidth;
-
-    if (width <= 767) {
-      setDeviceType('mobile');
-    } else if (width >= 768 && width <= 1024) {
-      setDeviceType('tablet');
-    } else {
-      setDeviceType('desktop');
+  const onChangeTelefono = (e) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value)) { // Solo permite números
+      setTelefono(value);
     }
   };
-  
-  const changeModalWarning = message => {
-    if(typeof message === "string"){
-      setDefineModal({
-        status: true,
-        title: "Advertencia",
-        content: message
-      })
-    }
-  } 
 
-  const handlerVDate = value => {
-    const message = validDate(value)
-    changeModalWarning(message)
-  }
-
-  const handlerVFullName = value => {
-    const message = validFullName(value)
-    changeModalWarning(message)
-  }
-
-  const handlerVPhone = value => {
-    const message = validPhone(value)
-    changeModalWarning(message)
-  }
-
-  const handlerVCodeOrNuip = value => {
-    const message = validCodeOrNuip(value)
-    changeModalWarning(message)
-  }
-
-  const handlerVPlanOrArea = value => {
-    const message = validPlanOrArea(value)
-    changeModalWarning(message)
-  }
-
-  const handlerVSemester = value => {
-    const message = validSemester(value)
-    changeModalWarning(message)
-  }
-
-  const handlerVGender = value => {
-    const message = validListEmpty(cbxGender, value,"Debe elegir un Género")
-    changeModalWarning(message)
-  }
-
-  const handlerVDiagnostic = value => {
-    const message = validListEmpty(cbxDiagnostic, value, "Debe elegir un tipo de diagnóstico")
-    changeModalWarning(message)
-  }
-
-  const handlerCloseModal = () => {
-    setDefineModal( obj => ({
-      ...obj,
-      status: false
-    }))
-  }
-
-  useEffect(() => {
-    handleResize();
-
-    // Añade el event listener para cambios en el tamaño de la pantalla
-    window.addEventListener('resize', handleResize);
-
-    // Cleanup al desmontar el componente
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [])
+  const onChangeConduca = (e) => {
+    setConduca(e.target.value); // Actualiza el estado con el valor del input
+  };
 
 
-  let isMobile = deviceType === "mobile"
-  // let enableResponsive = isMobile || deviceType === "tablet"
-
-  return(
+  return (
     <>
-      <HeaderNurse/>
-      <Modal open={defineModal.status} onClose={handlerCloseModal}>
-        <Flex vertical gap={30}>
-          <Flex justify="center" align="center">
-            <span>{defineModal.title}</span>
-          </Flex>
-            <p>{defineModal.content}</p>
-          <Flex justify="center" align="center">
-            <button onClick={handlerCloseModal} className="button-save">
-              Entendido
-            </button>
-          </Flex>
-        </Flex>
-      </Modal>
-      <div className={styles.contentTitle}>
-        <span>Registro de actividades</span>
-        <p>Aquí se podrán registrar las actividades de usuarios al servicio</p>
-      </div>
-      <Card className={styles.card}>
-      <Flex vertical justify="center" align="center">
-        <Flex className={styles.divider} vertical={isMobile} gap={30}>
-          <DateSpanish
-          title="Fecha"
-          isRenderAsteric
-          placeholder="Fecha de la visita"
-          required
-          className={inputs}
-          blur={e => handlerVDate(e.value)}  
-          />
-          <SmallInput
-          title="Nombre"
-          isRenderAsteric
-          placeholder="Nombre del paciente"
-          required
-          onBlur={e => handlerVFullName(e.currentTarget.value)}
-          />
-        </Flex>
-        <Flex className={styles.divider} vertical={isMobile} gap={30}>
-          <SmallInput
-          title="Teléfono"
-          isRenderAsteric
-          placeholder="Teléfono del paciente"
-          required
-          onBlur={e => handlerVPhone(e.currentTarget.value)}
-          />
-          <SmallInput
-          title="Código / cédula"
-          isRenderAsteric
-          placeholder="Código/cédula del paciente"
-          required
-          onBlur={e => handlerVCodeOrNuip(e.currentTarget.value)}
-          />
-        </Flex>
-        <Flex className={styles.divider} vertical={isMobile} gap={30}>
-          <SmallInput
-          title="Plan / área dependencia"
-          isRenderAsteric
-          placeholder="Plan / área dependencia"
-          required
-          onBlur={ e => handlerVPlanOrArea(e.currentTarget.value)}
-          />
-          <SmallInput
-          title="Semestre"
-          placeholder="Semestre del paciente"
-          onBlur={e => handlerVSemester(e.currentTarget.value)}
-          />
-        </Flex>
-        <Flex className={styles.divider} vertical={isMobile} gap={30}>
-          <ComboBox
-          title="Género"
-          isRenderAsteric
-          placeholder="Seleccione"
-          options={cbxGender}
-          className={comboboxes}
-          blur={e => handlerVGender(e.value)}
-          />
-          <ComboBox
-          title="Diagnóstico"
-          isRenderAsteric
-          placeholder="Seleccione"
-          options={cbxDiagnostic}
-          className={comboboxes}
-          blur={e => handlerVDiagnostic(e.value)}
-          />
-        </Flex>
-      </Flex>
-      <TextArea
-        title="Conducta"
-        labelClass={styles.textAlignLeft}
-        placeholder="Conducta de la actividad"
-        className={`${styles.textArea} ${styles.divider}`}
-        autoSize={{minRows:3, maxRows:8}}
-        required
-      />
-      <Flex justify="center" align="center" className={styles.divider} gap={30}>
-        <button className="button-save">Guardar</button>
-        <button className="button-cancel">Cancelar</button>
-      </Flex>
-      </Card>
-      <FooterProfessionals/>
+      <HeaderNurse />
+      <main className="becas-section" style={{ marginTop: '100px' }}>
+        <h1 className="text-xl font-bold" style={{ marginBottom: '12px' }}>Registro de Actividades</h1>
+
+        <p style={{ marginBottom: '6px' }}>Aquí se podrán registrar las actividades del usuario en el servicio</p>
+
+        <Card
+          bordered={true}
+          style={{
+            width: '100%',
+            maxWidth: '700px',
+            marginTop: '100px',
+            margin: '3px auto',
+            justifyContent: 'center'
+          }}
+        >
+          <Space direction="vertical" size={16} style={{ width: '95%' }}>
+            {/* Fecha y Código/Cédula */}
+            <Form layout="vertical">
+              <Row gutter={40}>
+                <Col span={12}>
+                  <Form.Item label="Fecha" labelAlign="left" required>
+                    <DatePicker locale={esLocale} onChange={onChangeFecha} style={{ width: '100%' }} />
+                  </Form.Item>
+                </Col>
+
+                <Col span={12}>
+                  <Form.Item label="Código/Cédula" labelAlign="left" required>
+                    <SearchInputR />
+                  </Form.Item>
+                </Col>
+
+                <Col span={12}>
+                  <Form.Item label="Nombre" labelAlign="left" required>
+                    <Input
+                      placeholder="Ingrese el nombre"
+                      value={nombre}
+                      onChange={(e) => setNombre(e.target.value)}
+                    />
+                  </Form.Item>
+                </Col>
+
+                <Col span={12}>
+                  <Form.Item label="Teléfono" labelAlign="left" required>
+                    <Input
+                      placeholder="Ingrese el teléfono"
+                      value={telefono}
+                      onChange={onChangeTelefono}
+                      maxLength={10} // Limita a 10 dígitos
+                    />
+                  </Form.Item>
+                </Col>
+
+                <Col span={12}>
+                  <Form.Item label="Plan/Área Dependencia" labelAlign="left" required>
+                    <Input
+                      placeholder="Ingrese el plan o área de dependencia"
+                      value={planDependencia}
+                      onChange={(e) => setPlanDependencia(e.target.value)}
+                    />
+                  </Form.Item>
+                </Col>
+
+                <Col span={12}>
+                  <Form.Item label="Semestre" labelAlign="left" required>
+                    <Input
+                      placeholder="Ingrese el semestre"
+                      value={semestre}
+                      onChange={(e) => setSemestre(e.target.value)}
+                    />
+                  </Form.Item>
+                </Col>
+
+                <Col span={12}>
+                  <Form.Item label="Género" labelAlign="left" required>
+                    <Select placeholder="Selecciona una opción" style={{ width: '100%' }}>
+                      <Option value="masculino">Masculino</Option>
+                      <Option value="femenino">Femenino</Option>
+                      <Option value="otro">Otro</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+
+                <Col span={12}>
+                  <Form.Item label="Diagnóstico" labelAlign="left" required>
+                    <Select placeholder="Selecciona una opción" style={{ width: '100%' }}>
+                      <Option value="colicos-menstruales">Cólicos menstruales</Option>
+                      <Option value="curacion">Curación</Option>
+                      <Option value="dolor-cabeza">Dolor de cabeza</Option>
+                      <Option value="dolor-estomacal">Dolor estomacal</Option>
+                      <Option value="dolor-muscular">Dolor muscular</Option>
+                      <Option value="malestar-general">Malestar general</Option>
+                      <Option value="mareos-desmayos">Mareos/desmayos</Option>
+                      <Option value="preservativos">Preservativos</Option>
+                      <Option value="otro">Otro</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+
+                <Col span={24}>
+                  <Form.Item label="Conducta" labelAlign="left">
+                    <Input.TextArea
+                      placeholder="Escribe aquí una descripción larga sobre la conduca"
+                      value={conduca}
+                      onChange={onChangeConduca}
+                      rows={4} // Número de filas visibles
+                    />
+                  </Form.Item>
+                </Col>
+
+              </Row>
+            </Form>
+          </Space>
+
+          <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+            <Button className="button-save" type="primary"> Guardar</Button>
+            <Button className="button-cancel">Cancelar</Button>
+          </div>
+        </Card>
+
+
+      </main>
+
     </>
   );
 }
