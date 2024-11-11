35a14a244d6285cfb57269add5f6674180d8bbbb
Luisafcc14 <165429963+Luisafcc14@users.noreply.github.com>
2024-11-11 13:52:39 -0500

    mensajes de error a campos obligatorios

--- src/pages/nurse/VisitsNurse.jsx
+++ src/pages/nurse/VisitsNurse.jsx
@@ -18,6 +18,9 @@ const VisitsNurse = () => {
   const [conducta, setConducta] = useState('');
   const [diagnostico, setDiagnostico] = useState('');
 
+  const [form] = Form.useForm();
+
+
   const diagnosticOptions = ["COLICOS_MENSTRUALES", "CURACION", "DOLOR_DE_CABEZA", "DOLOR_ESTOMACAL", "DOLOR_MUSCULAR", "MALESTAR_GENERAL", "MAREOS_DESMAYOS", "PRESERVATIVOS", "OTRO"];
   const genderOptions = ["MASCULINO", "FEMENINO", "OTRO", "NO_RESPONDE"];
 
@@ -31,6 +34,17 @@ const VisitsNurse = () => {
       setPlanDependencia(data.plan);
       setSemestre(data.semester);
       setGenero(data.gender);
+
+      // Actualizar los valores del formulario con los datos encontrados
+      form.setFieldsValue({
+        username: data.username,
+        nombre: data.name,
+        apellido: data.lastname,
+        telefono: data.phone,
+        planDependencia: data.plan,
+        semestre: data.semester,
+        genero: data.gender,
+      });
       message.success('Usuario encontrado');
     } catch {
       message.error('Usuario no registrado. Realice el registro para crearlo');
@@ -38,17 +52,34 @@ const VisitsNurse = () => {
   };
 
   const handleRegisterActivity = async () => {
-
-    
-    const payload = { date: fecha, username: username.trim(), name: nombre, lastname: apellido, phone: telefono, plan: planDependencia, semester: semestre, gender: genero.toUpperCase(), diagnostic: diagnostico, conduct: conducta };
-    try {
-      await api.post('/nursing-activities/register', payload);
-      message.success('Actividad registrada exitosamente');
-      resetFields();
-    } catch {
-      message.error('Ocurrió un error al registrar la actividad');
-    }
+    // Validar campos requeridos
+    form.validateFields()
+      .then(async () => {
+        const payload = {
+          date: fecha,
+          username: username.trim(),
+          name: nombre,
+          lastname: apellido,
+          phone: telefono,
+          plan: planDependencia,
+          semester: semestre,
+          gender: genero.toUpperCase(),
+          diagnostic: diagnostico,
+          conduct: conducta
+        };
+        try {
+          await api.post('/nursing-activities/register', payload);
+          message.success('Actividad registrada exitosamente');
+          resetFields();
+        } catch {
+          message.error('Ocurrió un error al registrar la actividad');
+        }
+      })
+      .catch(() => {
+        message.error('Por favor, complete todos los campos obligatorios');
+      });
   };
+
 
   const resetFields = () => {
     setFecha(null);
@@ -61,6 +92,7 @@ const VisitsNurse = () => {
     setGenero('');
     setConducta('');
     setDiagnostico('');
+    form.resetFields();  // Resetear el formulario de Ant Design
   };
 
   return (
@@ -71,37 +103,55 @@ const VisitsNurse = () => {
         <p style={{ marginBottom: '6px' }}>Aquí se podrán registrar las actividades de los usuarios en el servicio.</p>
         <Card bordered style={{ width: '100%', maxWidth: '700px', margin: '3px auto' }}>
           <Space direction="vertical" size={16} style={{ width: '95%' }}>
-            <Form layout="vertical">
+            <Form form={form} layout="vertical">
               <Row gutter={40}>
                 <Col span={12}>
-                  <Form.Item label="Fecha" required>
+                  <Form.Item label="Fecha" name="fecha"
+                    required
+                    rules={[{ required: true, message: 'La fecha es obligatoria' }]}
+                  >
                     <DatePicker locale={esLocale}
-                      onChange={(date) => setFecha(date)} 
+                      onChange={(date) => setFecha(date)}
                       value={fecha} style={{ width: '100%' }} />
                   </Form.Item>
                 </Col>
                 <Col span={12}>
-                  <Form.Item label="Código/Cédula" required>
+                  <Form.Item label="Código/Cédula" name="username"
+                    required
+                    rules={[{ required: true, message: 'El código/cedula es obligatorio' }]}
+                  >
                     <SearchInputR value={username} onSearch={() => handleSearchUser()} onChange={(e) => setUsername(e.target.value)} />
                   </Form.Item>
                 </Col>
                 <Col span={12}>
-                  <Form.Item label="Nombre" required>
+                  <Form.Item label="Nombre" name="nombre"
+                    required
+                    rules={[{ required: true, message: 'El nombre es obligatorio' }]}
+                  >
+
                     <Input placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
                   </Form.Item>
                 </Col>
                 <Col span={12}>
-                  <Form.Item label="Apellido" required>
+                  <Form.Item label="Apellido" name="apellido"
+                    required
+                    rules={[{ required: true, message: 'El apellido es obligatorio' }]}
+                  >
                     <Input placeholder="Apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} />
                   </Form.Item>
                 </Col>
                 <Col span={12}>
-                  <Form.Item label="Teléfono" required>
+                  <Form.Item label="Teléfono" name="telefono"
+                    required
+                    rules={[{ required: true, message: 'El teléfono es obligatorio' }]}
+                  >
                     <Input placeholder="Teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} maxLength={10} type="tel" />
                   </Form.Item>
                 </Col>
                 <Col span={12}>
-                  <Form.Item label="Plan/Área Dependencia" required>
+                  <Form.Item label="Plan/Área Dependencia" name="planDependencia"
+                    rules={[{ required: true, message: 'El plan de dependencia es obligatorio' }]}
+                  >
                     <Input placeholder="Plan o área de dependencia" value={planDependencia} onChange={(e) => setPlanDependencia(e.target.value)} />
                   </Form.Item>
                 </Col>
@@ -111,12 +161,18 @@ const VisitsNurse = () => {
                   </Form.Item>
                 </Col>
                 <Col span={12}>
-                  <Form.Item label="Género" required>
+                  <Form.Item label="Género" name="genero"
+                    required
+                    rules={[{ required: true, message: 'El género es obligatorio' }]}
+                  >
                     <Select value={genero} onChange={setGenero} options={genderOptions.map(option => ({ value: option, label: option }))} />
                   </Form.Item>
                 </Col>
                 <Col span={12}>
-                  <Form.Item label="Diagnóstico" required>
+                  <Form.Item label="Diagnóstico" name="diagnostico"
+                    required
+                    rules={[{ required: true, message: 'El diagnóstico es obligatorio' }]}
+                  >
                     <Select value={diagnostico} onChange={setDiagnostico} options={diagnosticOptions.map(option => ({ value: option, label: option }))} />
                   </Form.Item>
                 </Col>
@@ -133,7 +189,7 @@ const VisitsNurse = () => {
             <Button className='button-cancel' onClick={resetFields}>Cancelar</Button>
           </div>
         </Card>
-      </main>
+      </main >
     </>
   );
 };
