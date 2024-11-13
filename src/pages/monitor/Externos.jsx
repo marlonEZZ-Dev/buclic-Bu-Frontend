7a5e5a25d7b99f78c7f759d7df3176bf2731c10c
Luisafcc14 <165429963+Luisafcc14@users.noreply.github.com>
2024-11-13 18:40:10 -0500

    Cambios diseño

--- src/pages/monitor/Externos.jsx
+++ src/pages/monitor/Externos.jsx
@@ -329,7 +329,7 @@ const Externos = () => {
           </Space>
 
           <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
-            <Button className="button-save" onClick={handleSave}>Guardar</Button>
+            <Button className="button-save" onClick={handleSave}>Reservar</Button>
             <Button className="button-cancel" onClick={() => form.resetFields()}>Cancelar</Button>
           </div>
         </Card>
