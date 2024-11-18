211a4d07706b8107046373f53684fca2f64528df
marlonEZZ-Dev <duendecum@gmail.com>
2024-11-17 19:56:20 -0500

    permitiendo que en edición el funcionario pueda dejar vacío el campo de correo

--- src/pages/admin/ManagementUsers.jsx
+++ src/pages/admin/ManagementUsers.jsx
@@ -395,7 +395,7 @@ const handlePageChange = page => {
         username: validCode(user.username, !isFuncionary, isFuncionary),
         name: validName(user.name),
         lastName: validLastname(user.lastName),
-        email: validEmail(user.email, isFuncionary),
+        email: validEmail(user.email, isFuncionary, isModalEdit),
         plan: validPlan(user.plan),
         roles: validRol(user.roles),
         grant: !isBeneficiary ? true : validGrant(user.grant, isModalEdit)
@@ -617,7 +617,7 @@ useEffect(() => {
   if (pressedSave) {
       handlerOkValidation({
           name: "email",
-          value: validEmail(user.email, isFuncionary),
+          value: validEmail(user.email, isFuncionary, isModalEdit),
           fnState: setOkValidation
       });
   }
@@ -693,7 +693,7 @@ useEffect(() => {
   if (pressedEdit && objectSelected) {
       handlerOkValidation({
           name: "email",
-          value: validEmail(objectSelected.email, isFuncionary),
+          value: validEmail(objectSelected.email, isFuncionary, isModalEdit),
           fnState: setOkValidationEdit
       });
   }
