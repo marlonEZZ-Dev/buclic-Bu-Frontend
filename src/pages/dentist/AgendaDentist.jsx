09d30c95941d85fbf0fde5c2cc141e9543af2b0f
marlonEZZ-Dev <duendecum@gmail.com>
2024-11-17 20:44:23 -0500

    Es probable que se solucione

--- src/pages/dentist/AgendaDentist.jsx
+++ src/pages/dentist/AgendaDentist.jsx
@@ -219,7 +219,8 @@ export default function AgendaDentist() {
                 className={styles.searchInput}
                 placeholder="Fecha de consulta (dd/MM/yyyy)"
                 format={"DD/MM/YYYY"}
-                onChange={value => {setSearchDate(value); console.log(dayjs(value));}}
+                value={searchDate}
+                onChange={value => setSearchDate(value)}
               />
               <Button
               type="primary"
@@ -239,10 +240,16 @@ export default function AgendaDentist() {
               }}
               onMouseEnter={() => setHover(true)}
               onMouseLeave={() => setHover(false)}/>
-              <ButtonRefresh onClick={() => {
+              <ButtonRefresh onClick={ () => {
+                let verify = "Entro"
                 setSearchDate(""); // Limpia la búsqueda
+                verify += " - Limpio"
                 setCurrentPage(1); // Resetea la página al refrescar
+                verify += " - Refresco"
                 fetchAttendedAppointments(1);
+                verify += " - Cargo"
+                console.log(`sucedido ${verify}`)
+                console.log(`Esta vacio ? ${searchDate === "" ? true : searchDate}`)
               }}/>
             </Flex>
           </Flex>
