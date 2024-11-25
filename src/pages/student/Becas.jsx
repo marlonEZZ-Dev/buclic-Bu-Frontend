1881f9b7d3d2c38948ef19ef49ffd19c77357cf7
marlonEZZ-Dev <duendecum@gmail.com>
2024-11-24 21:27:44 -0500

    Agregando el boton a las vistas determinadas para estudiante

--- src/pages/student/Becas.jsx
+++ src/pages/student/Becas.jsx
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from 'react';
 import TopNavbar from '../../components/TopNavbar';
 import BecasReservation from '../../components/global/BecasReservation';
 import FooterProfessionals from "../../components/global/FooterProfessionals.jsx";
-
+import ButtonTutorial from '../../components/global/ButtonTutorial.jsx';
 const Becas = () => {
 
   return (
     <>
       <TopNavbar />
       <BecasReservation />
+      <ButtonTutorial role="student"/>
       <FooterProfessionals />
-
     </>
   );
 };
